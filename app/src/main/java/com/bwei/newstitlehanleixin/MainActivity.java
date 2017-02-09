4b6568df9963b0de7fc7983a21e8297965f2b41f
文氓 <leixinhan@foxmail.com>
2017-02-09 19:52:58 +0800

    Seve

--- app/src/main/java/com/bwei/newstitlehanleixin/MainActivity.java
+++ app/src/main/java/com/bwei/newstitlehanleixin/MainActivity.java
@@ -2,6 +2,7 @@ package com.bwei.newstitlehanleixin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,35 +10,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Oliver","onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d("Oliver","onStart");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d("Oliver","onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.d("Oliver","onStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.d("Oliver","onRestart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("Oliver","onResume");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d("Oliver","onDestroy");
     }
 }

da7f54da77e3e2b9699bde8910c76fa47fa9a0d4
xkwg <leixinhan@foxmail.com>
2017-02-10 21:01:51 +0800

    Oliver

--- app/src/main/java/com/bwei/newstitlehanleixin/MainActivity.java
+++ app/src/main/java/com/bwei/newstitlehanleixin/MainActivity.java
@@ -1,17 +1,94 @@
 package com.bwei.newstitlehanleixin;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.bwei.newstitlehanleixin.fragment.FragmentCare;
+import com.bwei.newstitlehanleixin.fragment.FragmentMine;
+import com.bwei.newstitlehanleixin.fragment.FragmentVideo;
+import com.bwei.newstitlehanleixin.fragment.FramentHome;
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
+
+    Button home;
+    Button video;
+    Button care;
+    Button mine;
+
+    RelativeLayout activity_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d("Oliver","onCreate并引入布局文件");
         setContentView(R.layout.activity_main);
 
-        Log.d("Oliver","onCreate");
+        //查找相关控件的方法
+        getID();
+
     }
 
+    //查找相关控件的方法
+    public void getID(){
+
+        home = (Button) findViewById(R.id.bth_main_home);
+        video = (Button) findViewById(R.id.bth_main_scover);
+        care = (Button) findViewById(R.id.bth_main_care);
+        mine= (Button) findViewById(R.id.bth_main_mine);
+
+        home.setOnClickListener(this);
+        video.setOnClickListener(this);
+        care.setOnClickListener(this);
+        mine.setOnClickListener(this);
+    }
+
+     public void selected(){
+
+         home.setSelected(false);
+         video.setSelected(false);
+         care.setSelected(false);
+         mine.setSelected(false);
+
+         home.setTextColor(getResources().getColor(R.color.main_button_select_before));
+         video.setTextColor(getResources().getColor(R.color.main_button_select_before));
+         care.setTextColor(getResources().getColor(R.color.main_button_select_before));
+         mine.setTextColor(getResources().getColor(R.color.main_button_select_before));
+
+     }
+
+    @Override
+    public void onClick(View view) {
+
+        selected();
+
+        switch (view.getId()){
+
+            case R.id.bth_main_home:
+                home.setSelected(true);
+                home.setTextColor(getResources().getColor(R.color.all_background_title));
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_main_framelayout,new FramentHome()).commit();
+                break;
+            case R.id.bth_main_scover:
+                video.setSelected(true);
+                video.setTextColor(getResources().getColor(R.color.all_background_title));
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_main_framelayout,new FragmentVideo()).commit();
+                break;
+            case R.id.bth_main_care:
+                care.setSelected(true);
+                care.setTextColor(getResources().getColor(R.color.all_background_title));
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_main_framelayout,new FragmentCare()).commit();
+                break;
+            case R.id.bth_main_mine:
+                mine.setSelected(true);
+                mine.setTextColor(getResources().getColor(R.color.all_background_title));
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_main_framelayout,new FragmentMine()).commit();
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/bwei/newstitlehanleixin/fragment/FragmentVideo.java
@@ -0,0 +1,32 @@
+package com.bwei.newstitlehanleixin.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bwei.newstitlehanleixin.R;
+
+/**
+ * Author ${韩磊鑫} on 2017/2/10 13:37
+ * 邮箱：leixinhan@foxmail.com
+ * 项目名称：
+ * 类描述：
+ * 修改人：${Oliver}
+ * 修改备注：
+ * 修改时间：
+ */
+
+public class FragmentVideo extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = View.inflate(getActivity(), R.layout.frament_video,null);
+        return view;
+    }
+}
